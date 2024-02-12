575829143f6503835563a8c761cdcb196116aeb3
rjimeno <roberto.jimeno@gmail.com>
2024-02-11 19:43:06 -0500

    Testing 'terraform fmt' via Actions.

--- TF/variables.tf
+++ TF/variables.tf
@@ -2,7 +2,7 @@
 # it should contain a single line similar to the following:
 # region = "us-east-1"
 variable "region" {
-  description = "The region Terraform deploys your instance"
+description = "The region Terraform deploys your instance"
   type        = string
   nullable    = false
   default     = "us-east-1"
